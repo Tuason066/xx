59672eb4ac6a5a8a7d57a36657b44753df7fb546
jeffrey <jeffrey@geniihut.com>
2024-08-27 10:31:26 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,29 @@
+const WebSocket = require('ws');
+const chokidar = require('chokidar');
+const path = require('path');
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+});
+
+const frontendPath = path.join(__dirname, '../frontend');
+
+const watcher = chokidar.watch(frontendPath, {
+  ignored: /(^|[\/\\])\../, // ignore dotfiles
+  persistent: true,
+  ignoreInitial: true, // ignore initial add events
+});
+
+watcher.on('change', (path) => {
+  console.log(`${path} has been changed`);
+
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send('reload');
+    }
+  });
+});
+
+console.log('WebSocket server is running on ws://localhost:8080');
